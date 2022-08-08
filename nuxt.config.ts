e0668138213a87816830b9c0f0473c307204dd63
novellac <38117965+novellac@users.noreply.github.com>
2022-08-07 21:31:39 -0400

    init: Add Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/x-icon', href: 'public/favicon.ico' }],
+    },
+  },
+})
