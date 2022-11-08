293302304384b5ec79129799dc80f7b002b5ce2c
novellac <38117965+novellac@users.noreply.github.com>
2022-11-08 09:04:46 -0500

    feat: Add ability to show individual pages
    Caveat: Does not link those pages anywhere yet.

--- content/events/event-2022-08-august-community-meeting.md
+++ content/events/event-2022-08-august-community-meeting.md
@@ -5,3 +5,5 @@ eventend: 2022-08-11T23:00:25.486Z
 location: 11435 Morang Ave.
 description: Join us for our monthly community meeting.
 ---
+
+Some great full content
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts',
+    '@nuxt/content'
   ],
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.scss'
