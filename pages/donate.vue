58cc7b44c16f56550c30aad0bf146bdedc5d745f
novellac <38117965+novellac@users.noreply.github.com>
2024-08-03 13:07:28 -0400

    feat: Add donation page

---
+++ pages/donate.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="px-6">
+    <div class="mb-8">
+      <h1>Donate</h1>
+      <p>
+        All donations go directly to helping the Yorkshire Woods neighborhood grow and thrive!
+        <br>
+        Yorkshire Woods Community Organization is a 501(c)(3) nonprofit, and donations are tax-deductible and non-refundable.
+        <a href="https://www.zeffy.com/en-US/fundraising/296cf943-b89e-4ff8-859b-952cc4ddf186">Donations made online</a>
+        will receive a donation receipt.
+      </p>
+    </div>
+
+    <section class="flex flex-col gap-8 mb-8">
+      <div>
+        <h2>Residents and individuals</h2>
+        <p>Residents and individuals are encouraged to donate $120 per year ($10 per month).</p>
+        <a href="https://www.zeffy.com/en-US/fundraising/296cf943-b89e-4ff8-859b-952cc4ddf186">Make your donation
+          online</a>
+      </div>
+      <div>
+        <h2>Businesses</h2>
+        <p>Businesses are encouraged to donate $500 per year ($42 per month).</p>
+        <a href="https://www.zeffy.com/en-US/fundraising/296cf943-b89e-4ff8-859b-952cc4ddf186">Make your donation
+          online</a>
+      </div>
+    </section>
+  </article>
+</template>
