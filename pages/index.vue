7e51a0d62a25b31889054d3f2a5065d6287bd805
novellac <38117965+novellac@users.noreply.github.com>
2024-12-23 13:42:51 -0500

    chore: thaw fund featured resource

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,62 @@
     <h1 class="px-6 mb-8 text-center">Welcome to the neighborhood!</h1>
 
     <section class="flex flex-col p-6">
-      <h2 id="featured-event">Featured event</h2>
-      <p class="max-w-md mb-8">
-        Bring the whole family and come join us at the Harvest Festival. October 26, from 12 - 4pm, at 9745
-        Kensington Ave. Enjoy trick or treat bags, vendors, resources, and more!
-      </p>
-      <h3 class="sr-only">Event images</h3>
+      <h2 id="featured-event">Featured Resource</h2>
+      <div id="thawFundDescrip" class="flex flex-col gap-4 mb-8">
+        <p>
+          The Heat and Warmth (THAW) Fund has opened its program to the Yorkshire Woods neighborhood! THAW fund program
+          was awarded a $400k for sustainable home repairs for residents in Yorkshire Woods only. <br>
+          Recipients will be awarded anywhere from $500 - $35,000 per household for safety and health repairs. <br>
+          <span class="bg-green-100 block p-4 m-4">
+            THAW fund is taking applications from Yorkshire Woods neighborhood residents for grants to help with health
+            and safety repairs for homeowners. Applications will be open December 1. Awards are on a first come, first
+            serve basis for Yorkshire Woods residents. Severity of repair for safety and health purposes will be
+            prioritized.
+          </span>
+
+          Repairs must be for safety needs and sustainability, not beautification projects. Examples of eligible home
+          repairs include cement and brick damage.<br>
+          Home repair program includes the following:
+        </p>
+
+        <ul class="list-disc list-inside">
+          <li>Roofs, soffit, gutters, and fascia (no partial replacements)</li>
+          <li>Storm doors/Exterior doors</li>
+          <li>Windows</li>
+          <li>Insulation</li>
+          <li>Modification for modernization/efficiency purposes or replacement of heating (furnaces or boilers)/
+            cooling (full home only)/ventilation systems and water heater upgrades including on demand retrofitting</li>
+          <li>Functioning and up to date exterior security lighting</li>
+          <li>Appliance upgrades to energy star rating i.e. stove, refridgerator</li>
+          <li>Electrical updates or replacement - wiring of home (partial or full)</li>
+        </ul>
+
+        <p>
+          Other eligible activities requiring MSHDA pre-approval and cannot be stand-alone:
+        </p>
+
+        <ul class="list-disc list-inside">
+          <li>Masonry replcement or repair</li>
+          <li>Lead and/or asbestos remediation</li>
+          <li>Interior walls, ceilings, and drywall</li>
+          <li>Porch/deck repair and installation</li>
+          <li>Private driveways and sidewalks (concrete and asphalt)</li>
+        </ul>
+
+        <p>
+          To be eligible, you must own your home, meet the AMI income requirement, complete household income
+          self-certification, submit identification, proof of income, and proof of home ownership and insurance. <br>
+          Interested? Contact THAW fund at <a href="tel:18008668429">1-800-866-8429</a> or find THAW fund online at <a
+            href="www.thawfund.org">www.thawfund.org</a>.
+        </p>
+      </div>
+      <div class="flex gap-4 flex-wrap sm:flex-nowrap mb-12 w-full">
+        <img class="basis-1/2 sm:w-1/2 shrink object-contain" src="~/public/images/uploads/thawFund20241219.jpg"
+          alt="THAW fund flyer page 1" aria-describedby="thawFundDescrip" />
+        <img class="basis-1/2 sm:w-1/2 shrink object-contain" src="~/public/images/uploads/thawFund20241219b.jpg"
+          alt="THAW fund flyer page 2" aria-describedby="thawFundDescrip" />
+      </div>
+      <!-- <h3 class="sr-only">Event images</h3>
       <ul class="flex flex-wrap justify-center gap-6 mb-10">
         <li>
           <figure>
@@ -17,7 +67,7 @@
             alt="A brightly colored flyer with cartoon hay, pumpkins, corn, and jack-o-lanterns. Flyer advertises the harvest festival being held on October 26 from 12pm - 4pm at 9745 Kensington Ave." />
           </figure>
         </li>       
-      </ul>
+      </ul> -->
 
       <EventsList title="Upcoming Events" eventTimeframe="future" theme="green" />
       <NuxtLink class="ml-auto text-lg underline" to="/events">See all events</NuxtLink>
@@ -37,12 +87,13 @@
 
       <h3 class="mt-3 mb-1">Anchor Detroit survey</h3>
       <p>
-        Anchor Detroit, presented by <a href="https://liferemodeled.org">Life Remodeled</a>, is a community center in the Yorkshire Woods area.
+        Anchor Detroit, presented by <a href="https://liferemodeled.org">Life Remodeled</a>, is a community center in
+        the Yorkshire Woods area.
         We encourage you to fill out their survey about your needs for Anchor Detroit. This will help
         Life Remodeled bring the programs and events YOU want to see!
       </p>
       <a href="https://submit.jotform.com/beckyLR/anchordetroitsurvey">Fill out the Anchor Detroit survey here!</a>
-      
+
       <h3 class="mt-6 mb-1">Yorkshire Woods Neighborhood survey</h3>
       <p>
         Fill out our community survey to tell us what you need from your
