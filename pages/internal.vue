9adf733182a588daf54073cd08f1ddd73dd01f0b
novellac <38117965+novellac@users.noreply.github.com>
2025-06-27 09:44:21 -0400

    feat: Add "internal" page for public but unlisted board resources

---
+++ pages/internal.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="px-6">
+    <div class="mb-8">
+      <h1>Internal Resources</h1>
+      <p>These resources aren't a secret, but they are for internal use.</p>
+      <p>If you're interested in contributing, <NuxtLink to="/contact">join us!</NuxtLink> We are always looking for interested residents to participate in meetings, head up block clubs, and even sit on the board!</p>
+    </div>
+
+    <section class="mb-8">
+      <h2>Door hangers</h2>
+      <ul>
+        <li>
+          <a href="/door-hager" target="_blank">Canva file for door hanger (opens in new tab)</a>
+        </li>
+        <li>
+          <a href="/images/uploads/YWCODoorHanger20250627.pdf" target="_blank">PDF - current as of June 2025 (opens in new tab)</a>
+        </li>
+      </ul>
+    </section>
+  </article>
+</template>
+
+<script setup />
